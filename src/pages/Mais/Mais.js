6032a56eec685365a89f99a49c0c63c67c4ee6b7
Bedê <bedegabriel14@gmail.com>
2023-08-19 13:09:18 -0300

    expo location

--- src/pages/Mais/Mais.js
+++ src/pages/Mais/Mais.js
@@ -1,21 +1,114 @@
-import { StyleSheet, Text, View, Image, ScrollView, StatusBar, SafeAreaView, Platform} from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import * as Location from 'expo-location';
 
-const statusBarHeight = StatusBar.currentHeight;
+export default function App() {
+  const [mapRegion, setMapRegion] = useState({
+    latitude: -23.4545815,
+    longitude: -46.5425497,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
 
-export default function Mais() {
+  });
+
+  const userLocation = async () => {
+    let {status} = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted'){
+      setErrorMsg('Permitir acessos à localização');
+    }
+    let location = await Location.getCurrentPositionAsync({enableHighAccuracy: true});
+    setMapRegion({
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 0.01,
+      longitudeDelta: 0.01,
+    });
+    console.log(location.coords.latitude, location.coords.longitude);
+  }
+
+  useEffect (() => {
+    userLocation();
+  }, []);
+
   return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView>
-        <Text>Section Mais</Text>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={styles.container}>
+     {/*Header do aplicativo*/}
+        <View style={styles.Header}>
+          <Image style={styles.Perfil}/>
+                        
+          <TouchableOpacity>
+            <Image style={styles.Logo} source={require('../../img/Logo.png')} />
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => navigation.navigate('Perfil')}>
+            <Image style={styles.Perfil} source={require('../../img/Perfil.png')} />
+          </TouchableOpacity>
+        </View>
+
+      <MapView style={styles.map} region={mapRegion}>
+      <Marker coordinate={mapRegion} title='Sua localização atual' />
+      </MapView>
+
+          <TouchableOpacity onPress={userLocation}>
+            <Text style = {styles.Botao} value={''}> Cadastrar vulnerável</Text>
+          </TouchableOpacity>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: Platform.OS === 'android' ? statusBarHeight : 25,
+    backgroundColor: '#fff'
+  },
+
+  Header:{
+    width:'100%',
+    flexDirection: 'row',
+    justifyContent:'space-between',
+    marginTop:-34.5,
+    marginBottom:-20,
+    alignSelf:'center',
+    alignItems:'center',
+  },
+
+  Logo:{
+    width:30,
+    resizeMode:'contain',
+    alignSelf: 'center',
+  },
+
+  Perfil:{
+    width:50,
+    resizeMode:'contain',
+    alignSelf: 'center',
+    marginRight:25,  
+  },
+
+  map: {
+    marginTop:-30,
+    width: '100%',
+    height: '60%',
+  },
+
+  Botao:{
+    margin:25,
+    backgroundColor: "black",
+    borderRadius: 8,
+    padding: 13,
+    color: 'white',
+    fontFamily: 'Montserrat_700Bold',
+    fontSize: 15,
+    marginTop: 30,
+    textAlign:'center',
+    shadowColor: '#black',
+    shadowOpacity: 0.1,
+    shadowOffset: {
+    width: 0,
+    height: 2,
+    },
+    shadowRadius: 4,
+    elevation: 3, 
   },
 });
